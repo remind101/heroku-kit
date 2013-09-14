190b81c344cd24b21a0647c5d9b4ab5e2c0f8133
Eric J. Holmes <eric@ejholmes.net>
2013-09-14 02:18:39 +0000

    Stuff

--- lib/heroku/kit.rb
+++ lib/heroku/kit.rb
@@ -1,3 +1,14 @@
+require 'formatted-metrics'
+require 'collective-metrics'
+require 'rack-timeout'
+require 'request_id'
+
+begin
+  require 'sidekiq'
+rescue LoadError
+  # No Sidekiq
+end
+
 require 'heroku/kit/version'
 require 'heroku/kit/railtie' if defined?(Rails)
 require 'heroku/kit/sidekiq' if defined?(Sidekiq)
--- lib/heroku/kit/railtie.rb
+++ lib/heroku/kit/railtie.rb
@@ -1,8 +1,8 @@
 module Heroku
   module Kit
     class Railtie < ::Rails::Railtie
-      initializer 'heroku-kit.middleware' do
-        app.middleware.insert 0, Rack::RequestId
+      initializer 'heroku-kit.middleware' do |app|
+        app.middleware.insert_after Rack::Timeout, Rack::RequestId
         app.middleware.insert_after Rack::RequestId, Rack::Instrumentation
       end
     end
